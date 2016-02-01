413b8a4cfa5cf8feb2d3d099cc8718958974fdb6
sshik1 <sshikha2@sapient.com>
2016-02-01 16:29:46 +0530

    update

--- Java4/HandsOn/src/com/training/polymorphism/RentalApp.java
+++ Java4/HandsOn/src/com/training/polymorphism/RentalApp.java
@@ -11,30 +11,10 @@ public class RentalApp {
 	}
 
 	public static void main(String[] args) {
-
-		//		int input;
-//		Fan f = new Fan(1,670.00,2,"Orion","Table fan");
-//		Vessels v = new Vessels(1,450.00,3,"Bone China","small");
-//		//f.getDetails(1);
-//		System.out.println("Enter serial num (1/2): ");
-//		
-//		Scanner in = new Scanner(System.in);
-//		input = in.nextInt();
-//		if(input==1){
-//			f.getDetails(input);
-//			System.out.println("Rent is :  "  + f.getRent(f.getRate(),f.getDays()));
-//	}
-//		else if(input==2){
-//			v.getDetails(input);
-//			System.out.println("Rent is : "  + v.getRent(v.getRate(),f.getDays()));
-//			}
-//		else System.out.println("Wrong input");
-//		in.close();
 		
 		int days;
 		int itemNo;
-		//int input;
-		//Items i[]= new Items[2];
+	
 		
 		Scanner in = new Scanner(System.in);
 		
@@ -59,7 +39,6 @@ public class RentalApp {
 			case 2:
 				{
 					Items i = new Vessels(itemNo,450.00,days,"Bone China","small"); 
-					//out.println("\nRent at 40% rate : "  + v.getRent(v.getRate(),v.getDays()));
 					printDetails(i);
 				}
 			break;
