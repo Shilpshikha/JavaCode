53e465784487f6aa1f9bb695c2d9b78d44fd84e8
sshik1 <sshik1@delbahamas46645.sapient.com>
2016-03-30 16:45:13 +0530

    Node js first example

---
+++ NodeFirstExample/TestNode.js
@@ -0,0 +1,19 @@
+/**
+ * http://usejsdoc.org/
+ */
+var http = require('http');
+	
+try{
+
+		http.createServer(function (request, response) {
+
+			response.writeHead(200, {'Content-Type': 'text/plain'});
+			response.end('Hello World\n');
+		}).listen(2020);
+		
+		console.log('Server started');
+
+	}
+catch(err){
+  console.log("Error");
+}
